1c7f1502cf191ec9080db288f1e8130d4762d3dc
Wesley Osorio <wesleyfosorio@hotmail.com>
2017-10-02 17:28:09 -0300

    Starting the evolutionary script and fixing some bugs

--- js/AI/playerAI.js
+++ js/AI/playerAI.js
@@ -1,26 +1,43 @@
-//-------- PLAYER AI SCRIPT --------------------------
 var timeOutControl = []
-var generations = 10
-var population = 50
+UsingAI = true // Setting this to not show alerts
+//I believe that variables will become maybe a field to be configuered by the user
+var generations = 2
+var population = 5
 var speed = 1000
+
+
 
 createTable() //Put this here to not be needed read the rules and click on the play button
 
 function startAutoPlay(){
-  player = new playerSpiece
-  player.construct( 0,0,0 )
-  var i = 0
-  while ( i < 10000 ) {
-    (function( i ) {
-      timeOutControl.push( setTimeout( function() {
-        alive = celClick( player.returnMove() )
-        if( alive == false ){
-          stopAutoPlay()
-        }
-    }, speed * i ))
-    })( i++ )
+  var arrPopulation = []
+  for( var genControl = 0; genControl < generations; genControl++ ){
+    arrPopulation.push([])
+    for( var popControl = 0; popControl < population; popControl++ ){
+      arrPopulation[genControl].push( new playerSpecimen )
+      arrPopulation[genControl][popControl].construct( genControl, popControl, 0 )
+      arrPopulation[genControl][popControl] = specimenPlay( arrPopulation[genControl][popControl] )
+      console.log( "Generation : "+ genControl + " Specimen Number: " + population + " Fitness: " + arrPopulation[genControl][popControl].fitness )
+    }
   }
+  console.log(arrPopulation)
 }
+
+
+function specimenPlay( specimen ){
+  for ( var i = 0; i < 10000; i++ ) {
+    timeOutControl = setInterval(function(){
+      specimen.fitness = score + moves
+      var alive = celClick( specimen.returnMove() )
+      if( alive == false ){
+        stopAutoPlay()
+        return specimen
+      }
+    }, speed)
+  }
+  return specimen
+}
+
 
 function stopAutoPlay(){
   for( var time in timeOutControl ){
@@ -45,10 +62,10 @@ function getEnviroment(){
     return enviroment
 }
 
-class playerSpiece{
-  construct( generation, specie, genome ){
+class playerSpecimen{
+  construct( generation, specimen, genome ){
     this.gen = generation
-    this.specie = specie
+    this.specimen = specimen
     this.genome = genome
     this.fitness = 0
     this.enviroment = getEnviroment()
@@ -77,14 +94,27 @@ class playerSpiece{
     this.enviroment = getEnviroment()
     var posWarrior = this.warriorPosition()
     var s = Math.random()
+
     if( s >= 0 && s < 0.25 ){ // up
       cel = String( parseInt( posWarrior ) - 10 )
+      if( posWarrior >= 11 && posWarrior < 20 ){ // Adding this to prevent innerHTML errors
+        cel = String( parseInt( posWarrior ) + 10 )
+      }
     }else if(s >= 0.25 && s < 0.5){ // down
       cel = String( parseInt( posWarrior ) + 10 )
+      if( posWarrior >= 90 ){ // Adding this to prevent innerHTML errors
+        cel = String( parseInt( posWarrior ) - 10 )
+      }
     }else if(s >= 0.5 && s < 0.75){ //left
       cel = String( parseInt( posWarrior ) - 1 )
+      if( posWarrior.substring( 1, 2 ) == 1 ){ // Adding this to prevent innerHTML errors
+        cel = String( parseInt( posWarrior ) + 1 )
+      }
     }else if(s >= 0.75 && s < 1){ //right
       cel = String( parseInt( posWarrior ) + 1 )
+      if( posWarrior.substring( 1, 2 ) == 9 ){ // Adding this to prevent innerHTML errors
+        cel = String( parseInt( posWarrior ) - 1 )
+      }
     }/*else if(s >= 0.8 && s < 1){ //pass turn
       cel = String( parseInt( posWarrior ) )
     }*/
